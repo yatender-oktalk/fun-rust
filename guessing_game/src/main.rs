49c064389f313c76a4d322e95ec3989dff5f911e
yatender <yatender@getvokal.com>
2019-02-11 23:52:36 +0530

    changing function using rand number generator module

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,12 +1,18 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
+
     println!("Please input your guess.");
 
     let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    io::stdin().read_line(&mut guess)
+    .expect("could not read please retry");
 
-     println!("You guessed: {}", guess);
+    println!("You guessed: {}", guess);
 }
