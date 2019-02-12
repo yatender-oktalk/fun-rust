b063370fef1d86f73f740f01f4e94c6d37127459
yatender <yatender@getvokal.com>
2019-02-12 10:16:39 +0530

    checking the string thing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,12 @@
+
 fn main() {
     let mut x: u32 = 5;
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+    let mut s = String::from("hello how are you");
+    s.push_str(", world!");
+    s.push('w');
+    println!("{}", s);
 }
