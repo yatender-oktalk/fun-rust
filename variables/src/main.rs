aeda6d12f509c383e4ce6dec368e625705169a16
yatender <yatender@getvokal.com>
2019-02-12 10:34:12 +0530

    understanding ownership

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -14,4 +14,20 @@ fn main() {
     let s2 = s1;
     println!("{}", s2);
 
+    let s3 = s2.clone();
+    println!("{}", s3);
+
+
+    let _s4 = gives_ownership();
+    let s5 = String::from("hello");
+    let _s6 = take_and_gives_back(s5);
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("hello");
+    some_string
+}
+
+fn take_and_gives_back(a_string: String) -> String {
+    a_string
 }
