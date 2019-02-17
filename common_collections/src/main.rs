1b7d87ff230be22abb123c8ca0130d0224ae2222
Yatender <yatender@getvokal.com>
2019-02-17 23:53:39 +0530

    updating a vector

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -2,5 +2,17 @@
 fn main() {
     let v: Vec<i32> = Vec::new();
     let v = vec![1,2,3,4,5];
-    println!("{:?}", v)
+    println!("{:?}", v);
+
+
+    //updating a vector
+    let mut v = Vec::new();
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+
+    println!("{:?}", v);
+
+    
 }
