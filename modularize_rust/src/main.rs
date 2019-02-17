6b1c50b0f13387519d751b0511b243d113313d22
Yatender <yatender@getvokal.com>
2019-02-17 21:42:13 +0530

    understanding use

--- modularize_rust/src/main.rs
+++ modularize_rust/src/main.rs
@@ -1,36 +1,36 @@
 mod sound{
 
     pub mod instrument {
-
-
         pub mod woodwind {
-            pub fn clarinet() {
+            pub fn clarinet() -> () {
                 super::super::super::breathe_in()
             }
         }
 
-        pub fn guitar() {
+        pub fn guitar() -> () {
             println!("guitar goes here")
         }
     }
 
     pub mod voice {
-        pub fn guitar() {
+        pub fn guitar() -> () {
             println!("voice guitar goes here")
         }
     }
 
 }
 
-fn breathe_in(){
-            println!("breathe_in goes here")
-        }
+use sound::instrument::woodwind;
+
+fn breathe_in() -> (){
+    println!("breathe_in goes here")
+}
 
 fn main() {
     println!("Hello, world!");
     // absolute path
     crate::sound::instrument::woodwind::clarinet();
     crate::sound::instrument::guitar();
-    sound::instrument::woodwind::clarinet();
+    woodwind::clarinet();
     sound::voice::guitar();
 }
