351b169b329fde8cbe62f790741f66724f71e55a
Yatender <yatender@getvokal.com>
2019-02-17 18:31:11 +0530

    adding pattern matching function

--- match_rust/src/main.rs
+++ match_rust/src/main.rs
@@ -1,3 +1,31 @@
+enum Coin{
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny =>1 {
+            println!("Luckey ");
+            1
+        },
+        Coin::Nickel =>5 {
+            println!("Luckey Nickel");
+            5
+        },
+        Coin::Dime =>10 {
+            println!("Luckey Dime");
+            10
+        },
+        Coin::Quarter =>25 {
+            println!("Luckey Quarter");
+            25
+        },
+    }
 }
