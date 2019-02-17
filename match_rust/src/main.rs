67ad05882c0674d11b2aa4c8e84e9c370dcf6e12
Yatender <yatender@getvokal.com>
2019-02-17 19:49:54 +0530

    underscore

--- match_rust/src/main.rs
+++ match_rust/src/main.rs
@@ -25,6 +25,15 @@ fn main() {
 
     println!("{:?}", five);
     println!("{:?}", six);
+
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => println!("something else"),
+    }
 }
 
 fn value_in_cents(coin: Coin) -> u32 {
