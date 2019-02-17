fb7659ad0a0ae7d3882408acd1c5a9880e60a157
Yatender <yatender@getvokal.com>
2019-02-17 21:18:55 +0530

    understanding modularization

--- modularize_rust/src/main.rs
+++ modularize_rust/src/main.rs
@@ -1,3 +1,29 @@
+mod sound{
+    pub mod instrument {
+        pub mod woodwind {
+            pub fn clarinet() {
+                println!("clarinet goes here")
+            }
+        }
+
+        pub fn guitar() {
+            println!("guitar goes here")
+        }
+    }
+
+    pub mod voice {
+        pub fn guitar() {
+            println!("voice guitar goes here")
+        }
+    }
+
+}
+
 fn main() {
     println!("Hello, world!");
+    // absolute path
+    crate::sound::instrument::woodwind::clarinet();
+    crate::sound::instrument::guitar();
+    sound::instrument::woodwind::clarinet();
+    sound::voice::guitar();
 }
