e2b85b475d2c34d0753666bbf912a93ebf7bfa25
Yatender <yatender@getvokal.com>
2019-02-19 10:53:17 +0530

    more string

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -51,4 +51,14 @@ fn main() {
     let s = "initial contents".to_string();
 
     println!("{}", s);
+
+    let mut s1 = String::from("foo");
+    let s2 = " bar";
+    s1.push_str(s2);
+    println!("s2 is {}", s2);
+
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2; // note s1 has been moved here and can no longer be used
 }
