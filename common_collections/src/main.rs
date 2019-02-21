108a972d589dad33e7fcbfc6db79036a0824556f
Yatender <yatender@getvokal.com>
2019-02-21 08:58:45 +0530

    adding iterator over text

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -80,6 +80,20 @@ fn main() {
     let team_name = String::from("Blue");
     let score = scores.get(&team_name);
 
+
+    scores.entry(String::from("Blue")).or_insert(50);
+    scores.entry(String::from("Yellow")).or_insert(50);
+
     println!("{:?}", scores);
 
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
 }
