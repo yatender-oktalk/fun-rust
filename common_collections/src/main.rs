e07ea611bb1ec24a85c150c9a047bc99081c452f
Yatender <yatender@getvokal.com>
2019-02-21 08:58:32 +0530

    one change

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -68,10 +68,18 @@ fn main() {
     scores.insert(String::from("Blue"), 10);
     scores.insert(String::from("Yellow"), 10);
 
-    let teams  = vec![String::from("Blue"), String::from("Yellow")];
-    let initial_scores = vec![10, 50];
+    // let teams  = vec![String::from("Blue"), String::from("Yellow")];
+    // let initial_scores = vec![10, 50];
 
-    //zipping values
-    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect()
-    
+    // //zipping values
+    // let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
+
+    println!("{:?}", scores);
+
 }
