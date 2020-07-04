a90abed2d0a4196481e7361bb8f79d94d574b2c3
yatender-oktalk <yatender.nitk@gmail.com>
2020-07-04 23:39:42 +0530

    adding vector

--- looper/src/main.rs
+++ looper/src/main.rs
@@ -1,16 +1,17 @@
 fn main() {
     println!("Hello, world!");
-    loop10();
+    loop_10();
     loop_for(5);
+    array_loop();
 }
 
-fn loop10() {
+fn loop_10() {
     let mut n = 0;
     loop {
         n += 1;
         println!("hello");
         if n >= 10 {
-            return; 
+            return;
         }
     }
 }
@@ -18,5 +19,16 @@ fn loop10() {
 fn loop_for(n: i32) {
     for i in 0..n {
         println!("{} yo", i);
+    }
+}
+
+fn array_loop() {
+    let mut v = Vec::new();
+    for i in 0..90 {
+        v.push(i)
+    }
+
+    for n in v {
+        println!("{}", n)
     }
 }
