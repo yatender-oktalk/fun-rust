5b4b524179787ebe1a4b68eb04566c4471ce40ac
yatender-oktalk <yatender.nitk@gmail.com>
2020-07-05 08:15:23 +0530

    more struct function added

--- play_with_struct/src/main.rs
+++ play_with_struct/src/main.rs
@@ -6,13 +6,31 @@ struct User{
     shoe_size: i8,
 }
 
+impl User{
+    fn simple_string(&self) -> String {
+        format!("{} - {} - {}cm - shoe_size: {}", self.name, self.age, self.height, self.shoe_size)
+    }
+
+    fn grow(&mut self, h: i32) {
+      self.height += h;
+    }
+
+    fn die(self) {
+      println!("Dead {}", self.simple_string());
+    }
+}
+
 fn main() {
-    let u = User{
+    let mut u = User{
         name: String::from("matt"),
         age: 32,
         height: 160,
         shoe_size: 8,
     };
 
-    println!("user is {:?}", u)
+    println!("user is {:?}", u.simple_string());
+    u.grow(20);
+    u.grow(20);
+    println!("user is {:?}", u.simple_string());
+    u.die()
 }
