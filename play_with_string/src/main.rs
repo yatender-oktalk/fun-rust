bac37972adaa61c742ffbabfdb702d8c925ccdfe
yatender-oktalk <yatender.nitk@gmail.com>
2020-07-05 07:16:06 +0530

    play with strings

--- play_with_string/src/main.rs
+++ play_with_string/src/main.rs
@@ -1,11 +1,25 @@
 fn main() {
     let s = String::from("Hello 大学生");
 
-    for c in s.chars() {
-        println!("{}",c);
+    println!("{}", s.len());
+
+    for (i, c) in s.char_indices() {
+        println!("{}, {}", c, i);
     }
 
     for c in s.bytes() {
-        println!("{}",c)
+        println!("{}", c)
     }
+
+    println!("{}", count_l(&s));
+}
+
+fn count_l(s: &str) -> i32 {
+    let mut res = 0;
+    for c in s.chars() {
+        if c == 'l' {
+            res += 1;
+        }
+    }
+    res
 }
