00be8b5dc60077d8acdad131ec4c4f6ec0292ed0
yatender-oktalk <yatender.nitk@gmail.com>
2020-07-16 11:56:12 +0530

    divided into modules

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,40 +1,11 @@
-pub mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-        fn seat_at_table() {}
-    }
+mod front_of_house;
 
-    mod serving {
-        fn take_order() {}
-        fn serve_order() {}
-        fn take_payment() {}
-    }
-}
-
-fn serve_order() {}
-
-mod back_of_house {
-    pub struct Breakfast {
-        pub toast: String,
-        seasonal_fruit: String,
-    }
-
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches"),
-            }
-        }
-    }
-}
+pub use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
-    let mut meal = back_of_house::Breakfast::summer("Rye");
-    println!("I'd like {} toast please", meal.toast);
-    meal.toast = String::from("Wheat");
-
-    println!("I'd like {} toast please", meal.toast);
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 }
 
 #[cfg(test)]
