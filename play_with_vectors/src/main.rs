b8994ecae9930dd00ec8b008e12ad791b4652bda
yatender-oktalk <yatender.nitk@gmail.com>
2020-07-16 12:22:35 +0530

    Adding values to vector

---
+++ play_with_vectors/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut v: Vec<i32> = Vec::new();
+    push_in_list(&mut v, 23);
+    println!("{:?}", v);
+}
+
+fn push_in_list(v: &mut Vec<i32>, number: i32) -> Vec<i32> {
+    v.push(number);
+    v.to_vec()
+}
