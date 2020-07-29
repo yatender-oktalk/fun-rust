c74fd4d88856bfd15ec5fd94ef597fc1dd6c77ef
yatender-oktalk <yatender.nitk@gmail.com>
2020-07-29 11:47:23 +0530

    create file if not exsists

--- panic/src/main.rs
+++ panic/src/main.rs
@@ -1,3 +1,18 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
 fn main() {
-    println!("Hello, world!");
+    let f = File::open("hello.txt");
+    let _f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(err) => panic!("unable to create file {:?}", err),
+            },
+            other_err => {
+                panic!("Problem in opening the file {:?}", other_err);
+            }
+        },
+    };
 }
