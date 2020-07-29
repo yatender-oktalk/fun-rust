4902fa7127c512f248fa6bc15a872fd70291b23f
yatender-oktalk <yatender.nitk@gmail.com>
2020-07-29 12:09:20 +0530

    understand unwrap or else

--- panic/src/main.rs
+++ panic/src/main.rs
@@ -1,18 +1,31 @@
 use std::fs::File;
 use std::io::ErrorKind;
 
+// fn main() {
+//     let f = File::open("hello.txt");
+//     let _f = match f {
+//         Ok(file) => file,
+//         Err(error) => match error.kind() {
+//             ErrorKind::NotFound => match File::create("hello.txt") {
+//                 Ok(fc) => fc,
+//                 Err(err) => panic!("unable to create file {:?}", err),
+//             },
+//             other_err => {
+//                 panic!("Problem in opening the file {:?}", other_err);
+//             }
+//         },
+//     };
+// }
+
 fn main() {
     let f = File::open("hello.txt");
-    let _f = match f {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(err) => panic!("unable to create file {:?}", err),
-            },
-            other_err => {
-                panic!("Problem in opening the file {:?}", other_err);
-            }
-        },
-    };
+    let _f = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("{:?}", error);
+            })
+        } else {
+            panic!("problem in opening in file {:?}", error)
+        }
+    });
 }
