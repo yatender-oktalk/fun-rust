3a47f6f7428e6a1fc945dadcf5a1d5f5cae9f362
yatender-oktalk <yatender.nitk@gmail.com>
2020-08-16 22:11:18 +0530

    formatting

--- gcd/src/main.rs
+++ gcd/src/main.rs
@@ -2,18 +2,17 @@ fn main() {
     let mut m: isize = 8;
     let mut n: isize = 2;
 
-    let resp = calculate_gcd(&mut m, &mut  n);
+    let resp = calculate_gcd(&mut m, &mut n);
     println!("{}", resp);
-
 }
 
-fn calculate_gcd(m: &mut isize,  n: &mut isize) -> isize {
+fn calculate_gcd(m: &mut isize, n: &mut isize) -> isize {
     loop {
         let rem = *m % *n;
         *m = *n;
         *n = rem;
         if *n == 0 {
-            break *m
+            break *m;
         }
     }
 }
