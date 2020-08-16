933e78fcac4a6b79f95d40606ad85e213ebe5db5
yatender-oktalk <yatender.nitk@gmail.com>
2020-08-16 22:51:32 +0530

    linear search

--- algorithms/linear_search/src/main.rs
+++ algorithms/linear_search/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn largest_i32(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
