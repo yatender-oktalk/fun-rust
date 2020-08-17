bb8cacfb5533b393df0c86ad030b9b3d859b18f5
yatender-oktalk <yatender.nitk@gmail.com>
2020-08-17 07:50:24 +0530

    Adding linear search

--- algorithms/linear_search/src/main.rs
+++ algorithms/linear_search/src/main.rs
@@ -1,7 +1,3 @@
-fn main() {
-    println!("Hello, world!");
-}
-
 fn largest_i32(list: &[i32]) -> i32 {
     let mut largest = list[0];
 
@@ -12,4 +8,26 @@ fn largest_i32(list: &[i32]) -> i32 {
     }
 
     largest
+}
+
+fn largest_char(list: &[char]) -> char {
+    let mut largest = list[0];
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 67, 89, 11, 23];
+    let result = largest_i32(&number_list);
+    println!("the largest number is {}", result);
+
+    let char_list = vec!['y', 'a', 'h', 'P'];
+
+    let result = largest_char(&char_list);
+    println!("the largest char is {}", result);
 }
