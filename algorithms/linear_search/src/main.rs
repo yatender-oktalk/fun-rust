867dfb268ac08728b86d26c3c53f3e3622b964b0
yatender-oktalk <yatender.nitk@gmail.com>
2020-08-22 08:57:22 +0530

    fixing the largest function

--- algorithms/linear_search/src/main.rs
+++ algorithms/linear_search/src/main.rs
@@ -21,7 +21,7 @@ fn largest_char(list: &[char]) -> char {
     largest
 }
 
-fn largest<T>(list: &[T]) -> T {
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
     let mut largest = list[0];
     for &item in list {
         if item > largest {
