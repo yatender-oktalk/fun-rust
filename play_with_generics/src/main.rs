586a977fa0491c660cf12ac0d2bde7d458d41124
yatender-oktalk <yatender.nitk@gmail.com>
2020-08-22 07:53:56 +0530

    Adding generics code

--- play_with_generics/src/main.rs
+++ play_with_generics/src/main.rs
@@ -1,3 +1,14 @@
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
 fn main() {
+    let integer = Point { x: 5, y: 10 };
+    println!("{}", integer.x);
+    println!("{}", integer.y);
+    let float = Point { x: 1.3, y: 3.2 };
+    println!("{}", float.y);
+    println!("{}", float.y);
     println!("Hello, world!");
 }
