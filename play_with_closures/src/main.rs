7c16405d191aea372c5532b14703fbff5edd8e7a
yatender-oktalk <yatender.nitk@gmail.com>
2020-08-26 11:43:03 +0530

    Adding a new generic struct and it's impl

--- play_with_closures/src/main.rs
+++ play_with_closures/src/main.rs
@@ -1,28 +1,59 @@
 use std::thread;
 use std::time::Duration;
 
+struct Cacher<T>
+where
+  T: Fn(u32) -> u32,
+{
+  calculation: T,
+  value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+where
+  T: Fn(u32) -> u32,
+{
+  fn new(calculation: T) -> Cacher<T> {
+    Cacher {
+      calculation,
+      value: None,
+    }
+  }
+
+  fn value(&mut self, arg: u32) -> u32 {
+    match self.value {
+      Some(v) => v,
+      None => {
+        let v = (self.calculation)(arg);
+        self.value = Some(v);
+        v
+      }
+    }
+  }
+}
+
 fn main() {
-    let simulated_user_specified_value = 18;
-    let simulated_random_number = 10;
-    generate_workout(simulated_user_specified_value, simulated_random_number);
+  let simulated_user_specified_value = 18;
+  let simulated_random_number = 10;
+  generate_workout(simulated_user_specified_value, simulated_random_number);
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_result = |num: u64| -> u32 {
-        println!("Doing some expensive calculation and generating your workout plan");
-        thread::sleep(Duration::from_secs(num));
-        intensity
-    };
+  let expensive_result = |num: u64| -> u32 {
+    println!("Doing some expensive calculation and generating your workout plan");
+    thread::sleep(Duration::from_secs(num));
+    intensity
+  };
 
-    if intensity < 25 {
-        let result = expensive_result(3);
-        println!("Today do {} pushups", result);
-        println!("After that do {} situps", result);
+  if intensity < 25 {
+    let result = expensive_result(3);
+    println!("Today do {} pushups", result);
+    println!("After that do {} situps", result);
+  } else {
+    if random_number == 3 {
+      println!("Take a break today! Remember to stay hydrated");
     } else {
-        if random_number == 3 {
-            println!("Take a break today! Remember to stay hydrated");
-        } else {
-            println!("Today do cardio for {} minutes", expensive_result(1));
-        }
+      println!("Today do cardio for {} minutes", expensive_result(1));
     }
+  }
 }
