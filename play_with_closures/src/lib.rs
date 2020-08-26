a54f21da47cf742caf3b33d8094cf8bb7955fe17
yatender-oktalk <yatender.nitk@gmail.com>
2020-08-26 13:31:39 +0530

    adding iterators

--- play_with_closures/src/lib.rs
+++ play_with_closures/src/lib.rs
@@ -8,6 +8,29 @@ fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
   shoes.into_iter().filter(|s| s.size == shoe_size).collect()
 }
 
+struct Counter {
+  count: u32,
+}
+
+impl Counter {
+  fn new() -> Counter {
+    Counter { count: 0 }
+  }
+}
+
+impl Iterator for Counter {
+  type Item = u32;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    if self.count < 5 {
+      self.count += 1;
+      Some(self.count)
+    } else {
+      None
+    }
+  }
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
@@ -47,5 +70,28 @@ mod tests {
         },
       ]
     )
+  }
+
+  #[test]
+  fn calling_next_directly() {
+    let mut counter = Counter::new();
+
+    assert_eq!(counter.next(), Some(1));
+    assert_eq!(counter.next(), Some(2));
+    assert_eq!(counter.next(), Some(3));
+    assert_eq!(counter.next(), Some(4));
+    assert_eq!(counter.next(), Some(5));
+    assert_eq!(counter.next(), None);
+  }
+
+  #[test]
+  fn using_other_iterator_trait_methods() {
+    let sum: u32 = Counter::new()
+      .zip(Counter::new().skip(1))
+      .map(|(a, b)| a * b)
+      .filter(|x| x % 3 == 0)
+      .sum();
+
+    assert_eq!(18, sum);
   }
 }
