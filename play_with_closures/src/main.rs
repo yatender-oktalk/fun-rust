f4d202e5ddb088099019d0fbc4cf4a736de51e88
yatender-oktalk <yatender.nitk@gmail.com>
2020-08-26 12:32:05 +0530

    Adding interator map

--- play_with_closures/src/main.rs
+++ play_with_closures/src/main.rs
@@ -32,39 +32,76 @@ where
   }
 }
 
-fn main() {
-  let simulated_user_specified_value = 18;
-  let simulated_random_number = 10;
-  generate_workout(simulated_user_specified_value, simulated_random_number);
+pub trait Iterator {
+  type Item;
+  fn next(&mut self) -> Option<Self::Item>;
 }
 
-fn generate_workout(intensity: u32, random_number: u32) {
-  let mut expensive_result = Cacher::new(|num| {
-    println!("Doing some expensive calculation and generating your workout plan");
-    thread::sleep(Duration::from_secs(3));
-    num
-  });
+fn main() {
+  // let simulated_user_specified_value = 18;
+  // let simulated_random_number = 10;
+  // generate_workout(simulated_user_specified_value, simulated_random_number);
 
-  if intensity < 25 {
-    let result = expensive_result.value(intensity);
-    println!("Today do {} pushups", result);
-    println!("After that do {} situps", result);
-  } else {
-    if random_number == 3 {
-      println!("Take a break today! Remember to stay hydrated");
-    } else {
-      println!(
-        "Today do cardio for {} minutes",
-        expensive_result.value(intensity)
-      );
-    }
+  let v1 = vec![1, 2, 3, 4];
+  let v1_iter = v1.iter();
+  for val in v1_iter {
+    println!("Got: {}", val);
   }
+
+  let v1: Vec<i32> = vec![1, 2, 3];
+
+  let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+  println!("{:?}", v2);
+}
+
+// fn generate_workout(intensity: u32, random_number: u32) {
+//   let mut expensive_result = Cacher::new(|num| {
+//     println!("Doing some expensive calculation and generating your workout plan");
+//     thread::sleep(Duration::from_secs(3));
+//     num
+//   });
+
+//   if intensity < 25 {
+//     let result = expensive_result.value(intensity);
+//     println!("Today do {} pushups", result);
+//     println!("After that do {} situps", result);
+//   } else {
+//     if random_number == 3 {
+//       println!("Take a break today! Remember to stay hydrated");
+//     } else {
+//       println!(
+//         "Today do cardio for {} minutes",
+//         expensive_result.value(intensity)
+//       );
+//     }
+//   }
+// }
+
+// #[test]
+// fn call_with_different_values() {
+//   let mut c = Cacher::new(|a| a);
+//   let v1 = c.value(1);
+//   let v2 = c.value(2);
+//   assert_eq!(v2, 2);
+// }
+
+#[test]
+fn iterator_demonstration() {
+  let v1 = vec![1, 2, 3];
+  let mut v1_iter = v1.iter();
+  assert_eq!(v1_iter.next(), Some(&1));
+  assert_eq!(v1_iter.next(), Some(&2));
+  assert_eq!(v1_iter.next(), Some(&3));
+  assert_eq!(v1_iter.next(), None);
 }
 
 #[test]
-fn call_with_different_values() {
-  let mut c = Cacher::new(|a| a);
-  let v1 = c.value(1);
-  let v2 = c.value(2);
-  assert_eq!(v2, 2);
+fn iterator_sum() {
+  let v1 = vec![1, 2, 3];
+
+  let v1_iter = v1.iter();
+
+  let total: i32 = v1_iter.sum();
+
+  assert_eq!(total, 6);
 }
